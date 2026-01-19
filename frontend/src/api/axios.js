e77a5126c67197c2f8a4a34ba87421e0f001ca98
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2026-01-20 02:17:08 +0530

    minor changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // "http://localhost:3000/api"
+  baseURL: "https://task-manager-mern-4znz.onrender.com/api", // "http://localhost:3000/api"
   withCredentials: true,
 });
 
